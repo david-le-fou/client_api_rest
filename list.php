3428762ffcd49e0e21cd5d3dea246c95d5b3e375
david.propitech <david.propitech@gmail.com>
2021-06-04 15:36:23 +0300

    master

---
+++ list.php
@@ -0,0 +1,15 @@
+<?php 
+$url = 'http://localhost:8000/produits.php?id=10';
+//  Initiate curl
+$ch = curl_init();
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,$url);
+// Execute
+$result=curl_exec($ch);
+// Closing
+curl_close($ch);
+
+// Will dump a beauty json :3
+var_dump($result);
